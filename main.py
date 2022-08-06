48cfd7f4a69ec1220e7de26c2322ef61f67fcc62
Harshal Nallapareddy <harshalreddyn@gmail.com>
2022-08-06 17:33:17 -0400

    changed image paths

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import tess
 import gui
 import algo
 
-text = tess.read_image("wordsearch.png")
-word_list = tess.read_image("words.png")
+text = tess.read_image("images/wordsearch.png")
+word_list = tess.read_image("images/words.png")
 word_board, word_list = gui.input_edit(text, word_list)
 word_coords = algo.solve(word_board, word_list)
 print(word_coords)
